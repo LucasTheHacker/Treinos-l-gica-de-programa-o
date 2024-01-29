c71e74d3bda1f8b4effe462809a2a5505e683e64
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-01-29 20:25:03 -0300

    Add files via upload

---
+++ Beecrowd 1004_Lucas.py
@@ -0,0 +1,10 @@
+'''Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD.
+A seguir mostre a variável PROD com mensagem correspondente'''
+
+A = int(input()) #A variável 'A' armazena esse valor
+
+B = int(input()) #A variável 'B' armazena o segundo valor
+
+print(f'PROD = {A * B}') #Poupa tempo do processador com a fstring
+
+print(' "MARKIM SOUZA" ')

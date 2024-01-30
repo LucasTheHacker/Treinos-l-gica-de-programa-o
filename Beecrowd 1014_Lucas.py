6fd31269a4eec65976ce4c04742079ed34c38a82
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-01-30 20:40:42 -0300

    Resolve o problema do input().split()

---
+++ Beecrowd 1014_Lucas.py
@@ -0,0 +1,9 @@
+#duas linhas de input - 1. Km / 2. litros de combustível
+#output - divide km por litros e mostra
+
+km = int(input())
+litros = float(input())
+
+print(f'{(km/litros):.3f} km/l')
+
+#tempo = 0s

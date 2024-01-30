0f1c6ddf9c514cef4ce1811da0d689c7fe6d7d94
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-01-29 21:02:09 -0300

    Add files via upload

---
+++ Beecrowd 1007_Lucas.py
@@ -0,0 +1,13 @@
+'''Leia quatro valores inteiros A, B, C e D. A seguir, 
+calcule e mostre a diferença do produto de A e B pelo produto de C e D 
+segundo a fórmula: DIFERENCA = (A * B - C * D).'''
+#Recebo as variáveis
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+DIFERENCA = (A * B - C * D)
+#calculo a diferença pela fórmula que o problema deu
+
+print(f'DIFERENCA = {DIFERENCA}')

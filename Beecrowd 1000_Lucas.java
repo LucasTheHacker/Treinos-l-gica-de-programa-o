122e53da1418276a05e327a196284c18a2ab0a88
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-02-03 17:05:28 -0300

    Primeiro programa em Java

---
+++ Beecrowd 1000_Lucas.java
@@ -0,0 +1,13 @@
+// O nome da classe tem que ser Main pra executar no Beecrowd
+
+import java.io.IOException;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+       System.out.println("Hello World!");
+ 
+    }
+ 
+}

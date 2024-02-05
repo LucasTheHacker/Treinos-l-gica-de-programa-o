104c57109e1b5a246f8055272471e59fce4f2790
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-02-05 17:53:32 -0300

    Add files via upload

---
+++ OlaMundo.java
@@ -0,0 +1,8 @@
+//PROJETO codigosjava
+package dr.lucas.batista.codigosjava;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+    }

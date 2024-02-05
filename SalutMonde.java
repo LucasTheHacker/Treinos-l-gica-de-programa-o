abebfc001c6297b465e93b5aec23df1e1fe26789
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-02-05 17:53:20 -0300

    Add files via upload

---
+++ SalutMonde.java
@@ -0,0 +1,10 @@
+//PROJETO codigosjava
+package dr.lucas.batista.codigosjava;
+
+public class SalutMonde {
+    public static void main(String[] args) {
+
+        System.out.println("Salut, Monde!");
+    }
+    
+}

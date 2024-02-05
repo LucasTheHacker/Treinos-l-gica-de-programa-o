303f57e052e6c2c27ebd9365232c2dd307bc9ee0
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-02-05 17:53:02 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//PROJETO codigosjava
+package dr.lucas.batista.codigosjava;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");  //print e println são diferentes (O print printa na tela e o println printa e vai pra linha de baixo)
+    }
+}

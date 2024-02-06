0454a1a2bcfa53937f44a6d8855f36aaf5d41f0e
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-02-06 14:47:46 -0300

    Bug resolvido. Explicação no código.

--- Beecrowd 1021_Lucas.py
+++ Beecrowd 1021_Lucas.py
@@ -1,8 +1,9 @@
 #Nível 6
 #Reaproveitando o código e a lógica do beecrowd 1018
+#Recebe um valor de ponto flutuante de dupla precisão e devolve a quantidade mínima de notas e moedas que representam aquele valor
 
 valor = float(input())
-valor_moedas = valor - (valor//1)
+valor_moedas = valor - (valor//1) #pego apenas o valor flutuante
 valor =  valor//1  #transformo pra inteiro somente pra facilitar os cálculos
 valor = int(valor)
 nota_100 = valor//100  #o operador // pega a parte inteira de uma divisão
@@ -33,32 +34,30 @@ print(f'{nota_50} nota(s) de R$ 50.00')
 print(f'{nota_20} nota(s) de R$ 20.00')
 print(f'{nota_10} nota(s) de R$ 10.00')
 print(f'{nota_5} nota(s) de R$ 5.00')
-print(f'{nota_2} nota(s) de R$ 2,00')
+print(f'{nota_2} nota(s) de R$ 2.00')
 
 moeda_050 = valor_moedas//0.5
 valor_moedas -= moeda_050 * 0.5
-moeda_050 = int(moeda_050)
    
 moeda_025 = valor_moedas//0.25
 valor_moedas -= moeda_025 * 0.25
-moeda_025 = int(moeda_025)
 
 moeda_010 = valor_moedas//0.1
 valor_moedas -= moeda_010 * 0.1
-moeda_010 = int(moeda_010)
 
 moeda_005 = valor_moedas//0.05
 valor_moedas -= moeda_005 * 0.05
-moeda_005 = int(moeda_005)
 
-moeda_001 = int(valor_moedas//0.01)
+moeda_001 = valor_moedas/0.01  #Mudei de // pra / e funcionou
 
 print("MOEDAS:")
-print(f'{moeda_100} moeda(s) de R$ 1.00')
-print(f'{moeda_050} moeda(s) de R$ 0.50')
-print(f'{moeda_025} moeda(s) de R$ 0.25')
-print(f'{moeda_010} moeda(s) de R$ 0.10')
-print(f'{moeda_005} moeda(s) de R$ 0.05')
-print(f'{moeda_001} moeda(s) de R$ 0.01')
+print(f'{moeda_100:.0f} moeda(s) de R$ 1.00')
+print(f'{moeda_050:.0f} moeda(s) de R$ 0.50')
+print(f'{moeda_025:.0f} moeda(s) de R$ 0.25')
+print(f'{moeda_010:.0f} moeda(s) de R$ 0.10')
+print(f'{moeda_005:.0f} moeda(s) de R$ 0.05')
+print(f'{moeda_001:.0f} moeda(s) de R$ 0.01') 
 
-
+#print(0.03//0.01) PROBLEMA IDENTIFICADO
+'''Por algum motivo, o python pega o valor inteiro da divisão acima
+como 2, de forma que é necessário fazer a divisão simples e converter pra inteiro.'''

a4f7103f1d4caad026180f25cf5b55e55f8cb711
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-02-06 14:30:41 -0300

    Arquivo havia subido desatualizado

--- ResolucaoTela.java
+++ ResolucaoTela.java
@@ -1,5 +1,16 @@
 package dr.lucas.batista.codigosjava;
 
+    import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class ResolucaoTela {
-    
+    public static void main(String[] args) {
+        Dimension dimensao = Toolkit.getDefaultToolkit().getScreenSize();  //tools é uma variável que contém o método getDefaultToolkit()
+        double largura = dimensao.getWidth();
+        double altura = dimensao.getHeight();
+        System.out.println("A resolução dessa tela é: ");
+        System.out.println(largura + " x " + altura);
 }
+}
+
+//dúvidas sobre a lógica dos métodos, atributos e etc. --> estudar
